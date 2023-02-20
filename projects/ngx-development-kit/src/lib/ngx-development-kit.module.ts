902f6ca8392e46ece738bc5b9052ff32ae560d3c
lodygin <lodigin.alexander@gmail.com>
2023-02-20 22:50:09 +0200

    feat: create ngx-development-kit library

---
+++ projects/ngx-development-kit/src/lib/ngx-development-kit.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NgxDevelopmentKitComponent } from './ngx-development-kit.component';
+
+
+
+@NgModule({
+  declarations: [
+    NgxDevelopmentKitComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    NgxDevelopmentKitComponent
+  ]
+})
+export class NgxDevelopmentKitModule { }
---
+++ projects/ngx-development-kit/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-development-kit
+ */
+
+export * from './lib/ngx-development-kit.service';
+export * from './lib/ngx-development-kit.component';
+export * from './lib/ngx-development-kit.module';
