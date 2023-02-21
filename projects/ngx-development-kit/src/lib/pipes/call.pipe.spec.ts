eff519a2194c0b847aadea36c1f516cae796c7c6
lodygin <lodigin.alexander@gmail.com>
2023-02-21 23:46:42 +0200

    test(pipes): write tests for the call pipe

--- projects/ngx-development-kit/src/lib/pipes/call.pipe.spec.ts
+++ projects/ngx-development-kit/src/lib/pipes/call.pipe.spec.ts
@@ -1,8 +1,36 @@
 import { CallPipe } from './call.pipe';
 
 describe('CallPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CallPipe();
+  let pipe!: CallPipe;
+
+  beforeEach(() => {
+    pipe = new CallPipe();
+  });
+
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it('should transform a value via provided function', () => {
+    const double = (value: number): number => value * 2;
+    const inputValue = 2;
+    const outputValue = double(2);
+
+    expect(pipe.transform(inputValue, double)).toBe(outputValue);
+  });
+
+  it('should transform a value via provided function and context', () => {
+    class ValueStore {
+      constructor(private value: string) {}
+
+      public getValue(): string {
+        return this.value;
+      }
+    }
+
+    const value = 'foo';
+    const foo = new ValueStore(value);
+
+    expect(pipe.transform(value, foo.getValue, foo)).toBe(value);
   });
 });
