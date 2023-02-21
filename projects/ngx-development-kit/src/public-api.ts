792036c18e227de4132c64ce38d39be2acfddd33
lodygin <lodigin.alexander@gmail.com>
2023-02-21 20:45:54 +0200

    feat(models): add identifiable interface

--- projects/ngx-development-kit/src/public-api.ts
+++ projects/ngx-development-kit/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ngx-development-kit
  */
 
-export * from './lib/ngx-development-kit.service';
-export * from './lib/ngx-development-kit.component';
-export * from './lib/ngx-development-kit.module';
+export { Identifiable } from '@root/models';
