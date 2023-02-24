8b2de6bfea61bac19ff0095678409303f50a4caa
lodygin <lodigin.alexander@gmail.com>
2023-02-24 08:40:40 +0200

    build: add destroy service to the public-api, add rxjs in the peer dependencies

--- projects/ngx-development-kit/src/public-api.ts
+++ projects/ngx-development-kit/src/public-api.ts
@@ -2,5 +2,5 @@
  * Public API Surface of ngx-development-kit
  */
 
-export { Identifiable } from './lib/models';
 export { CallPipe } from './lib/pipes';
+export { DestroyService } from './lib/services';
