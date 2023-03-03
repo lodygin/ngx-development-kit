9aa8bc6eb2f304176d8b8162e8aab2eee658a78a
lodygin <lodigin.alexander@gmail.com>
2023-03-03 22:00:29 +0200

    feat(directives): create ngx-track-by directive

--- projects/ngx-development-kit/src/public-api.ts
+++ projects/ngx-development-kit/src/public-api.ts
@@ -4,4 +4,4 @@
 
 export { NgxCallPipe } from './lib/pipes';
 export { NgxDestroyService } from './lib/services';
-export { NgxLetDirective } from './lib/directives';
+export { NgxLetDirective, NgxTrackByDirective } from './lib/directives';
