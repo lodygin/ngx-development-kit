5caf68c993930e5345c9665d59fca8fc97a0e286
lodygin <lodigin.alexander@gmail.com>
2023-03-05 16:32:16 +0200

    refactor(pipes): overload function of the call pipe

--- projects/ngx-development-kit/src/lib/pipes/ngx-call.pipe.ts
+++ projects/ngx-development-kit/src/lib/pipes/ngx-call.pipe.ts
@@ -5,6 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class NgxCallPipe implements PipeTransform {
+  public transform<T, H>(value: T, handler: (input: T) => H): H;
+  public transform<T, H, C>(value: T, handler: (input: T) => H, context: C): H;
   public transform<T, H, C>(value: T, handler: (input: T) => H, context: C | null = null): H {
     return handler.call(context, value);
   }
