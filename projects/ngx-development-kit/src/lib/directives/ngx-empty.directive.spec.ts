a26f38ccf5dc031ad6e53d8a27bd8dde728d54e0
lodygin <lodigin.alexander@gmail.com>
2023-03-05 16:35:34 +0200

    test(directives): create tests for the ngx-empty directive

---
+++ projects/ngx-development-kit/src/lib/directives/ngx-empty.directive.spec.ts
@@ -0,0 +1,54 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AsyncPipe, NgForOf } from '@angular/common';
+import { By } from '@angular/platform-browser';
+import { BehaviorSubject } from 'rxjs';
+
+import { NgxEmptyDirective } from './ngx-empty.directive';
+
+describe('NgxEmptyDirective', () => {
+  @Component({
+    template: `
+      <ng-container *ngFor="let item of items$ | async; ngxEmpty: emptyRef">
+        <p>{{ item }}</p>
+      </ng-container>
+
+      <ng-template #emptyRef>
+        <p>Empty</p>
+      </ng-template>
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+  })
+  class TestComponent {
+    public items$ = new BehaviorSubject<string[]>([]);
+  }
+
+  let fixture: ComponentFixture<TestComponent>;
+  let component: TestComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TestComponent],
+      imports: [NgForOf, AsyncPipe, NgxEmptyDirective],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should show paragraph with Empty text', () => {
+    expect(fixture.debugElement.queryAll(By.css('p')).length).toBe(1);
+    expect(fixture.debugElement.nativeElement.textContent).toBe('Empty');
+  });
+
+  it('should not interfere with *ngFor', () => {
+    component.items$.next(['1', '2', '3', '4']);
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.queryAll(By.css('p')).length).toBe(4);
+    expect(fixture.debugElement.nativeElement.textContent).toBe('1234');
+  });
+});
