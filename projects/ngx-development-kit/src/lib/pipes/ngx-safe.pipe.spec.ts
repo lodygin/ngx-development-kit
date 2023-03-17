31f9bb7e19b9f6675d3a4cbbcf1ebf8ca3051a3f
lodygin <lodigin.alexander@gmail.com>
2023-03-17 20:59:56 +0200

    test(pipes): create tests fot the ngx-safe pipe

---
+++ projects/ngx-development-kit/src/lib/pipes/ngx-safe.pipe.spec.ts
@@ -0,0 +1,60 @@
+import { NgxSafePipe, SafeType } from './ngx-safe.pipe';
+import { DomSanitizer } from '@angular/platform-browser';
+
+describe('NgxSafePipe', () => {
+  let pipe: NgxSafePipe;
+  let sanitizer: DomSanitizer;
+
+  beforeEach(() => {
+    sanitizer = jasmine.createSpyObj('DomSanitizer', [
+      'bypassSecurityTrustHtml',
+      'bypassSecurityTrustStyle',
+      'bypassSecurityTrustUrl',
+      'bypassSecurityTrustResourceUrl',
+    ]);
+    pipe = new NgxSafePipe(sanitizer);
+  });
+
+  it('should be created', () => {
+    expect(pipe).toBeTruthy();
+  });
+
+  it('should transform html', () => {
+    const value = '<p>Hello, World!</p>';
+    pipe.transform(value, 'html');
+
+    expect(sanitizer.bypassSecurityTrustHtml).toHaveBeenCalledWith(value);
+  });
+
+  it('should transform style', () => {
+    const value = 'color: red;';
+    pipe.transform(value, 'style');
+
+    expect(sanitizer.bypassSecurityTrustStyle).toHaveBeenCalledWith(value);
+  });
+
+  it('should transform url', () => {
+    const value = 'http://example.com';
+    pipe.transform(value, 'url');
+
+    expect(sanitizer.bypassSecurityTrustUrl).toHaveBeenCalledWith(value);
+  });
+
+  it('should transform resourceUrl', () => {
+    const value = 'http://example.com/resource';
+    pipe.transform(value, 'resourceUrl');
+
+    expect(sanitizer.bypassSecurityTrustResourceUrl).toHaveBeenCalledWith(value);
+  });
+
+  it('should return value if type is not recognized', () => {
+    const value = 'Hello, World!';
+    const result = pipe.transform(value, 'unknown' as SafeType);
+
+    expect(sanitizer.bypassSecurityTrustHtml).not.toHaveBeenCalled();
+    expect(sanitizer.bypassSecurityTrustStyle).not.toHaveBeenCalled();
+    expect(sanitizer.bypassSecurityTrustUrl).not.toHaveBeenCalled();
+    expect(sanitizer.bypassSecurityTrustResourceUrl).not.toHaveBeenCalled();
+    expect(result).toEqual(value);
+  });
+});
