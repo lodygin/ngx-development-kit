fffb6c15643be269f8a1334acdff4aef35dab57f
lodygin <lodigin.alexander@gmail.com>
2023-03-24 00:26:48 +0200

    test(validators): create tests for the url validator

--- projects/ngx-development-kit/src/lib/validators/ngx-validators.spec.ts
+++ projects/ngx-development-kit/src/lib/validators/ngx-validators.spec.ts
@@ -49,4 +49,56 @@ describe('NgxValidators', () => {
       });
     });
   });
+
+  describe('url', () => {
+    it('should error on an empty string', () => {
+      expect(NgxValidators.url(new FormControl(''))).toEqual({ url: true });
+    });
+
+    it('should error on null', () => {
+      expect(NgxValidators.url(new FormControl(null))).toEqual({ url: true });
+    });
+
+    it('should error on undefined', () => {
+      expect(NgxValidators.url(new FormControl(undefined))).toEqual({
+        url: true,
+      });
+    });
+
+    it('should error on a non-url string', () => {
+      expect(NgxValidators.url(new FormControl('not url'))).toEqual({ url: true });
+    });
+
+    it('should error on an array', () => {
+      expect(NgxValidators.url(new FormControl([1, 2, 3]))).toEqual({ url: true });
+    });
+
+    it('should error on an object', () => {
+      expect(NgxValidators.url(new FormControl({}))).toEqual({ url: true });
+    });
+
+    it('should error on numbers', () => {
+      expect(NgxValidators.url(new FormControl(123456789))).toEqual({ url: true });
+    });
+
+    it('should not error on an link with https protocol', () => {
+      expect(NgxValidators.url(new FormControl('https://www.google.com/'))).toBeNull();
+    });
+
+    it('should not error on an link with http protocol', () => {
+      expect(NgxValidators.url(new FormControl('http://www.google.com/'))).toBeNull();
+    });
+
+    it('should not error on an link without protocol', () => {
+      expect(NgxValidators.url(new FormControl('google.com'))).toBeNull();
+    });
+
+    it('should error on an link with mailto protocol', () => {
+      expect(NgxValidators.url(new FormControl('mailto:jen@oreilly.com'))).toEqual({ url: true });
+    });
+
+    it('should not error on an link with ftp protocol', () => {
+      expect(NgxValidators.url(new FormControl('ftp://host.example.txt'))).toBeNull();
+    });
+  });
 });
